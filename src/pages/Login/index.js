44b867fce961dc2128abed6bafc71db1ae77f786
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-22 10:04:25 -0300

    feat: Adding the console.log in the SignUp and Login file in order to inspect the application behavior

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { LoginForm, CustomMessage, CustomHeader } from '../../components';
 
-const Login = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <CustomHeader message="Log-in to your account" />
-      <LoginForm />
-      <CustomMessage>
-        New to us? <Link to='/signup'>Sign Up</Link>
-      </CustomMessage>
-    </Grid.Column>
-  </Grid>
-);
+class Login extends Component {
+  render () {
+    console.log("props", this.props)
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <CustomHeader message="Log-in to your account" />
+          <LoginForm />
+          <CustomMessage>
+            New to us? <Link to='/signup'>Sign Up</Link>
+          </CustomMessage>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+};
 
 export default Login;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { CustomMessage, CustomHeader, SignUpForm } from '../../components';
 
-const SignUp = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <CustomHeader message="Log-in to your account" />
-      <SignUpForm />
-      <CustomMessage>
-        Already have an acount? <Link to='/'>Sign In</Link>
-      </CustomMessage>
-    </Grid.Column>
-  </Grid>
-);
-
+class SignUp extends Component {
+  render () {
+    console.log('props', this.props.history);
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <CustomHeader message="Log-in to your account" />
+        <SignUpForm />
+        <CustomMessage>
+          Already have an acount? <Link to='/'>Sign In</Link>
+        </CustomMessage>
+      </Grid.Column>
+    </Grid>
+    );
+  }
+}
+  
 export default SignUp;
