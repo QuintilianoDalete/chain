4fddde890457d276b1a537161b101597a1492562
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-22 14:36:46 -0300

    feat: Conecting the input data from SignUpform index.js  with the state

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react'
+import PropTypes from "prop-types";
 
-const SignUpForm = () => (
+const SignUpForm = ({
+  formData: { name, password, email },
+  onInputChange,
+  onHandleSubmit
+}) => (
+  
   <Form size='large'>
     <Segment stacked>
-      <Form.Input fluid placeholder='User Name' />
-      <Form.Input fluid placeholder='E-mail address' />
-      <Form.Input fluid placeholder='Password' type='password' />
-      <Button color='teal' fluid size='large'>
+      <Form.Input
+        value={name}
+        name="name"
+        fluid
+        placeholder="Name"
+        onChange={(event) => onInputChange(event)}
+      />
+      <Form.Input
+        value={email}
+        name="email"
+        fluid 
+        placeholder='E-mail address'
+        onChange={(event) => onInputChange(event)}
+      />
+      <Form.Input
+        value={password}
+        name="password"
+        fluid
+        placeholder='Password'
+        type='password'
+        onChange={(event) => onInputChange(event)}
+      />
+      <Button onChange={onHandleSubmit} color='blue' fluid size='large'>
         Register
       </Button>
     </Segment>
   </Form>
 );
+
+SignUpForm.propTypes = {
+  formData: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+  }).isRequired,
+  onInputChange: PropTypes.func.isRequired
+};
 
 export default SignUpForm;
