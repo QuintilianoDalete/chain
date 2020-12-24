45ab0a0349113694ee4659f2ee39401e9286fe52
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-24 17:34:52 -0300

    feat: Instaled the dependency uuid, added example tasks to the initial state

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import CustomMessage from './CustomMessage';
 import LoginForm from './LoginForm';
 import NavBar from './NavBar';
 import SignUpForm from './SignUpForm';
+import TaskGroup from './TaskGroup';
+import TaskCard from './TaskCard';
 
-export { SignUpForm, CustomHeader, CustomMessage, NavBar, LoginForm };
+export { TaskCard, TaskGroup, SignUpForm, CustomHeader, CustomMessage, NavBar, LoginForm };
--- src/store/ducks/initialState.js
+++ src/store/ducks/initialState.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default {
   auth: {
     usersList: [
@@ -8,6 +10,65 @@ export default {
       }
     ],
     currentUser: {},
-    inLogged: true
-  }
+    isLogged: true
+  },
+  tasks: [
+    {
+      id: uuidv4(),
+      title: "Testando um titilo",
+      subtitle: "testando um subtitulo",
+      description:
+        "essa é a descrição de uma task de teste para usarmos como mock"
+    },
+    {
+      id: uuidv4(),
+      title: "Testando um titilo",
+      subtitle: "testando um subtitulo",
+      description:
+        "essa é a descrição de uma task de teste para usarmos como mock"
+    },
+    {
+      id: uuidv4(),
+      title: "Testando um titilo",
+      subtitle: "testando um subtitulo",
+      description:
+        "essa é a descrição de uma task de teste para usarmos como mock"
+    },
+    {
+      id: uuidv4(),
+      title: "Testando um titilo",
+      subtitle: "testando um subtitulo",
+      description:
+        "essa é a descrição de uma task de teste para usarmos como mock"
+    },
+    {
+      id: uuidv4(),
+      title: "Testando um titilo",
+      subtitle: "testando um subtitulo",
+      description:
+        "essa é a descrição de uma task de teste para usarmos como mock"
+    },
+    {
+      id: uuidv4(),
+      title: "Testando um titilo",
+      subtitle: "testando um subtitulo",
+      description:
+        "essa é a descrição de uma task de teste para usarmos como mock"
+    },
+    {
+      id: uuidv4(),
+      title: "Testando um titilo",
+      subtitle: "testando um subtitulo",
+      description:
+        "essa é a descrição de uma task de teste para usarmos como mock"
+    },
+    {
+      id: 8,
+      title: "Testando um titilo",
+      subtitle: "testando um subtitulo",
+      description:
+        "essa é a descrição de uma task de teste para usarmos como mock"
+    }
+  ]
+
 };
