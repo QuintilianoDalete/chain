e5c84e37220cf9a4dd4575c80b604f050960b8f5
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-24 01:30:30 -0300

    feat: Added the logout feature to our nav bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { NavBar } from '../../components';
+import * as authAction from "../../store/ducks/auth/actions";
+
 
 class Home extends Component {
   render(){
+    console.log("props", this.props)
     return(
       <>
-        <NavBar />
+        <NavBar onLogOut={this.props.onLogOut} />
         <h1>Home</h1>
       </>
     );
   }
 }
 
-export default Home;
+const mapDispatchToProps = {
+  onLogOut: authAction.logOut
+};
+
+export default connect(null, mapDispatchToProps)(Home);
