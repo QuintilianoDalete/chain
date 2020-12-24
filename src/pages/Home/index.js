ec0119d22bf07ee5e6899347dd5a9a5553ba9825
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-24 01:07:02 -0300

    feat: Adding the nav bar to out home page, added an exemple user to our initial state

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { NavBar } from '../../components';
+
+class Home extends Component {
+  render(){
+    return(
+      <>
+        <NavBar />
+        <h1>Home</h1>
+      </>
+    );
+  }
+}
+
+export default Home;
