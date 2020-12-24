904b95f8294a050894a2643426d3939293f766ee
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-24 00:42:24 -0300

    feat: Adding the private route on the file routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Login, SignUp } from '../pages';
+import PrivateRoute from './PrivateRoutes';
 
 const Routes = () => (
   <Switch>
     <Route exact path='/chain' component={Login} />
-    <Route path='/home' component={() => <h1>Welcome the Chain</h1>} /> 
+    <PrivateRoute path='/home' component={() => <h1>Welcome the Chain</h1>} /> 
     <Route path='/signup' component={SignUp} />
   </Switch>
 );
