f67068e27b5033b31298b71d49680747918156d3
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-26 00:17:42 -0300

    fix: Ajusting the custom header and the custom message

--- src/components/CustomHeader/index.js
+++ src/components/CustomHeader/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Header } from "semantic-ui-react";
 
-const CustomHeader = ({ message, ...props }) => (
-  <Header {...props}>{message}</Header>
+const CustomHeader = ({ message }) => (
+  <Header as="h2" color="blue" textAlign="center">
+    {message}
+  </Header>
 );
 
 CustomHeader.propTypes = {
