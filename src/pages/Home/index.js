2135a8ac0ae94b56520f911cd69971146a16b665
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-26 00:22:59 -0300

    fix: Ajusting the home and login pages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Container, Header, Icon } from 'semantic-ui-react';
-import { TaskGroup } from '../../components';
+import React, { Component } from "react";
+import { Container, Header, Icon } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { TaskGroup } from "../../components";
 
 class Home extends Component {
-  render(){
-    const { tasks, auth: { currentUser: { name } } } = this.props;
-    return(
-      <Container fluid textAlign='justified' style={{ width: 1000}}>
-        <Header style={{ marginTop: 40 }} textAlign='center' as='h2'>
-          <Icon name='tasks' />
-          <Header.Content>{`${name}'s user Tasks`}</Header.Content>
+  render() {
+    const {
+      tasks,
+      auth: {
+        currentUser: { name }
+      }
+    } = this.props;
+
+    return (
+      <Container fluid textAlign="justified" style={{ width: 1000 }}>
+        <Header style={{ marginTop: 40 }} textAlign="center" as="h2">
+          <Icon name="tasks" />
+          <Header.Content>{`${name}'s Tasks`}</Header.Content>
         </Header>
         <TaskGroup tasks={tasks} />
       </Container>
@@ -18,9 +24,9 @@ class Home extends Component {
   }
 }
 
-const mapStateToProps = (tasks, auth) => ({
-  tasks,
-  auth
+const mapStateToProps = (state) => ({
+  tasks: state.tasks,
+  auth: state.auth
 });
 
 export default connect(mapStateToProps)(Home);
