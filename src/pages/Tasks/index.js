8757d53c003b025bbc2622d4c833aebfb0dcc1b4
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-26 00:13:32 -0300

    fix: Ajusting the tasks component and the PrivateRoutes.js file

--- src/pages/Tasks/index.js
+++ src/pages/Tasks/index.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { Container, Header, Icon } from 'semantic-ui-react';
-import { TaskForm,  TaskCard } from '../../components';
-import * as taskAction from '../../store/ducks/tasks/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import { Container, Header, Icon } from "semantic-ui-react";
+import { TaskForm, TaskCard } from "../../components";
+import * as tasksAction from "../../store/ducks/tasks/actions";
 
 class Tasks extends Component {
   state = {
-    title: '',
-    subtitle: '',
-    description: ''
+    title: "",
+    subtitle: "",
+    description: ""
   };
 
-  handleInputchange = ({ target: { name, value} }) => {
+  handleInputChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value });
   };
 
@@ -20,15 +20,15 @@ class Tasks extends Component {
     const { addTask, history } = this.props;
     const task = { id: uuidv4(), ...this.state };
     addTask(task);
-    history.push('/home');
+    history.push("/home");
   };
 
-  render(){
-    return(
+  render() {
+    return (
       <Container fluid textAlign="justified" style={{ width: 1000 }}>
         <Header style={{ marginTop: 40 }} textAlign="center" as="h2">
           <Icon name="tasks" />
-          <Header.Content>Add Task</Header.Content>
+          <Header.Content>Adicionar Task</Header.Content>
         </Header>
         <TaskCard task={this.state} fluid />
         <TaskForm
@@ -42,7 +42,7 @@ class Tasks extends Component {
 }
 
 const mapDispatchToProps = {
-  addTask: taskAction.addTask
+  addTask: tasksAction.addTask
 };
 
 export default connect(null, mapDispatchToProps)(Tasks);
--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { NavBar } from '../components';
-import * as authAction from '../store/ducks/auth/actions';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { NavBar } from "../components";
+import * as authAction from "../store/ducks/auth/actions";
 
 const PrivateRoute = ({ component: Component, auth, logOut, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={(props) => 
-        auth.isLogged ? (
+      render={(props) =>
+        auth.isLogged === true ? (
           <>
             <NavBar onLogOut={logOut} {...props} />
             <Component {...props} />
           </>
         ) : (
-          <Redirect to='/chain' />
+          <Redirect to="/" />
         )
       }
     />
@@ -27,7 +27,7 @@ PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
 };
 
-const mapStatetoProps = (state) => ({
+const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
@@ -35,4 +35,4 @@ const mapDispatchToProps = {
   logOut: authAction.logOut
 };
 
-export default connect(mapStatetoProps, mapDispatchToProps)(PrivateRoute);
+export default connect(mapStateToProps, mapDispatchToProps)(PrivateRoute);
