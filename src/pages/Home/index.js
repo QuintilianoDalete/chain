2005357052c0ff1d5ff7a3c1f1605a18ec9a9be6
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-25 22:57:56 -0300

    feat: Conecting the home with the store

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Container, Header, Icon } from 'semantic-ui-react';
 import { TaskGroup } from '../../components';
 
 class Home extends Component {
   render(){
+    console.log(this.props.tasks);
     return(
       <Container fluid textAlign='justified' style={{ width: 1000}}>
         <Header style={{ marginTop: 40 }} textAlign='center' as='h2'>
@@ -16,4 +18,9 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => ({
+  tasks: state.tasks,
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Home);
